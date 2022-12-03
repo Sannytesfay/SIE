50cd084d968149935deded65c852bc5697cc671b
sannytesfay <tesfay.sunny@gmail.com>
2022-12-03 12:18:11 -0600

    Sanny's 4 commit

--- app/src/main/java/com/example/android_appv2/ActualClubHome.java
+++ app/src/main/java/com/example/android_appv2/ActualClubHome.java
@@ -8,13 +8,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ActualClubHome extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton addPostButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
